7c005660ac9eb0cd6a6e11b38c1c3ad4b161e7cd
TroiKM <karentroiano2009@gmail.com>
2014-06-16 19:12:49 -0430

    PracticaSem11

---
+++ Practicas/Semana11/USBRendering/scenes/tomaRender01.bat
@@ -0,0 +1,9 @@
+# "C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 16 -cam camToma01 -s 1 -e 120 -proj  "E:/Mis Documentos/maya/projects/USBRendering" "E:/Mis Documentos/maya/projects/USBRendering/scenes/toma01_03.mb"
+
+ "C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 16 -cam camToma03 -s 1 -e 80 -proj  "E:/Mis Documentos/maya/projects/USBRendering" "E:/Mis Documentos/maya/projects/USBRendering/scenes/toma01_03.mb"
+
+ "C:/Program Files/Autodesk/Maya2014/bin/render.exe" -r mr -v 4 -rt 16 -cam camToma02 -s 1 -e 48 -proj  "E:/Mis Documentos/maya/projects/USBRendering" "E:/Mis Documentos/maya/projects/USBRendering/scenes/toma02.mb"
+
+ 
+ 
+
